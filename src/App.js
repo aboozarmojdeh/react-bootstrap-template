f25f0f429f653580c7d16377c89d23bc7e29e7e7
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-07-23 12:14:46 -0700

    first version of template

---
+++ src/App.js
@@ -0,0 +1,253 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container-fluid main-container">
+        <main>
+          <div className='row'>
+            <div className='col-md-9 col-sm-12 col-12'>
+              <div>
+                <a href='https://www.google.com' target='_blank' rel='noopener noreferrer'>
+                  <div className='webflow-banner'>
+                    <img src="./img/banner.png" width="25px" height="25px" alt="" />
+                    <div className='banner-text'>Enter your banner text here</div>
+                  </div>
+                </a>
+                <div className='pinned-apis'>
+                  <div className='row'>
+
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='pinned-card-heading'>
+                        <h4><span role='img' aria-label="pin">📌</span> Featured APIs of the week</h4>
+                      </div>
+                    </div>
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='card'>
+                        {/* <a className='card-link' href='/hunter-2494'></a> */}
+                        <a className='card-link' href='https://yahoo.com'>
+                          <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <div>
+                              <h6>Hunter</h6>
+                              <small>Data Access</small>
+                            </div>
+
+                          </div>
+                        </a>
+                        <div className='card-body'>
+                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='card'>
+                        {/* <a className='card-link' href='/hunter-2494'></a> */}
+                        <a className='card-link' href='https://yahoo.com'>
+                          <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <div>
+                              <h6>Hunter</h6>
+                              <small>Data Access</small>
+                            </div>
+
+                          </div>
+                        </a>
+                        <div className='card-body'>
+                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='card'>
+                        {/* <a className='card-link' href='/hunter-2494'></a> */}
+                        <a className='card-link' href='https://yahoo.com'>
+                          <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <div>
+                              <h6>Hunter</h6>
+                              <small>Data Access</small>
+                            </div>
+
+                          </div>
+                        </a>
+                        <div className='card-body'>
+                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='card'>
+                        {/* <a className='card-link' href='/hunter-2494'></a> */}
+                        <a className='card-link' href='https://yahoo.com'>
+                          <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <div>
+                              <h6>Hunter</h6>
+                              <small>Data Access</small>
+                            </div>
+
+                          </div>
+                        </a>
+                        <div className='card-body'>
+                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className='col-md-4 col-sm-6 col-12'>
+                      <div className='card'>
+                        {/* <a className='card-link' href='/hunter-2494'></a> */}
+                        <a className='card-link' href='https://yahoo.com'>
+                          <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <div>
+                              <h6>Hunter</h6>
+                              <small>Data Access</small>
+                            </div>
+
+                          </div>
+                        </a>
+                        <div className='card-body'>
+                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                        </div>
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <br />
+                <hr className='home-hr' />
+                <div className='divider-title'>
+                  <span>Recently Added</span>
+                </div>
+              </div>
+              <br />
+              <div className='row'>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-md-4 col-sm-6 col-12'>
+                  <div className='card'>
+                    {/* <a className='card-link' href='/hunter-2494'></a> */}
+                    <a className='card-link' href='https://yahoo.com'>
+                      <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
+                        <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                        <div>
+                          <h6>Hunter</h6>
+                          <small>Data Access</small>
+                        </div>
+
+                      </div>
+                    </a>
+                    <div className='card-body'>
+                      <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='pagination-div'></div>
+            </div>
+            <div className='col-md-3 col-sm-12 col-12'>b</div>
+          </div>
+
+        </main>
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
