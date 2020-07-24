a61706f0e4067dd00214e72a50f8b20673e5757e
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-07-24 14:44:08 -0700

    searchBox added and activated

--- src/App.js
+++ src/App.js
@@ -9,29 +9,56 @@ import Sidebar1 from './components/Sidebar1';
 import Sidebar2 from './components/Sidebar2';
 import Sidebar3 from './components/Sidebar3';
 import Newsletter from './components/Newsletter';
+import SearchBox from './components/SearchBox';
+import CardList from './components/CardList';
+
+
 
 
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      cats: [],
+      searchField: ''
+
+    }
+  }
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response=>response.json())
+  .then(data=>this.setState({cats:data}))
+ 
+}
+
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value })
+    console.log(this.state)
+    console.log(event.target.value)
+  }
+
   render() {
+    const filteredCats=this.state.cats.filter((cat)=>{
+      return (
+        cat.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    )
+    })
     return (
       <div className="App container-fluid main-container">
         <main>
           <div className='row'>
             <div className='col-md-9 col-sm-12 col-12'>
               <div>
-                  <Banner />
-                 <div className='pinned-apis'>
-                  <div className='row'>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-
-
-                  </div>
+                <Banner />
+                <SearchBox onSearchChange={this.onSearchChange} />
+                <div className='pinned-apis'>
+                  
+                    <CardList cats={filteredCats}/>
+                   
                 </div>
                 <br />
                 <Hr />
@@ -39,21 +66,22 @@ class App extends Component {
               </div>
               <br />
               <div className='row'>
-               <Card />
-               <Card />
-               <Card />
-               <Card />
-               <Card />
-               <Card />
-                </div>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+              </div>
               <Pagination />
             </div>
             <div className='col-md-3 col-sm-12 col-12'>
-       <Sidebar1 />
+              <Sidebar1 />
               <Newsletter />
               <Sidebar2 />
               <Sidebar3 />
-              
+
             </div>
           </div>
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const Card=()=>{
+const Card=(props)=>{
     return(
 
 <div className='col-md-4 col-sm-6 col-12'>
                       <div className='card'>
-                        <a className='card-link' href='https://yahoo.com'>
+                        <a className='card-link' href={`https://${props.website}`} target='_blank' rel="noopener noreferrer">
                           <div className="card-box card-header" style={{ backgroundColor: 'rgba(241, 169, 160, 0.5)' }}>
-                            <img src="https://logo.clearbit.com/hunter.io" className="card-image" width="30" loading="lazy" alt="Hunter" />
+                            <img src={`https://robohash.org/set_set4/${props.id}?200x200`} className="card-image" width="30" loading="lazy" alt={props.name} />
                             <div>
-                              <h6>Hunter</h6>
-                              <small>Data Access</small>
+                              <h6>{props.name}</h6>
+                              <small>{props.companyName}</small>
                             </div>
 
                           </div>
                         </a>
                         <div className='card-body'>
-                          <p className="card-text">Hunter API makes it easy to find or verify professional email addresses.</p>
+                          <p className="card-text">{props.description}</p>
                         </div>
                       </div>
                     </div>
